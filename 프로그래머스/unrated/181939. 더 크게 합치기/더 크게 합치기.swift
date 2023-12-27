2f17b51e033bed6151a95444783e4e7a474ae6eb
황민채 <103357078+Hminchae@users.noreply.github.com>
2023-12-27 14:32:21 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.swift
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int) -> Int {
-    let aStr: String = String(a)
-    let bStr: String = String(b)
-    
-    let result: Int = Int(aStr + bStr)! >=  Int(bStr + aStr)! ? Int(aStr + bStr)! : Int(bStr + aStr)!
-    return result
+    return 0
 }
