bf8439e4107eabfa6330d540407f1a089a836c0e
황민채 <103357078+Hminchae@users.noreply.github.com>
2024-03-12 23:44:01 +0900

    [Bronze V] Title: 나머지, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.swift
@@ -0,0 +1,9 @@
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+let A = arr[0]
+let B = arr[1]
+let C = arr[2]
+
+print("\((A + B) % C)")
+print("\(((A%C) + (B%C))%C)")
+print("\((A*B)%C)")
+print("\(((A%C) * (B%C))%C)")
