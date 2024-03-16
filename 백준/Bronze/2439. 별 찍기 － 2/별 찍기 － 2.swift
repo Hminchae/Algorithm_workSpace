7116f17df3fc9f5f8b938a6319ecbafa95a3b3e9
황민채 <103357078+Hminchae@users.noreply.github.com>
2024-03-16 14:39:12 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.swift
@@ -0,0 +1,10 @@
+let N = Int(readLine()!)!
+for i in (1...N).reversed(){
+    for _ in 1..<i{
+        print(" ",terminator: "")
+    }
+    for _ in 1...(N-i+1){
+        print("*",terminator: "")
+    }
+    print()
+}
