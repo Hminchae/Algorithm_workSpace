1a8a4b0a7d4d64c2da35a1e639620ed779dc1559
황민채 <103357078+Hminchae@users.noreply.github.com>
2024-04-21 00:02:55 +0900

    [Bronze II] Title: 상수, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.swift
+++ 백준/Bronze/2908. 상수/상수.swift
@@ -1,17 +1,5 @@
-let number = readLine()!.split(separator: " ").map {String($0)}
-let num1 = Array(number[0])
-let num2 = Array(number[1])
-var str1 : String = ""
-var str2 : String = ""
+let input = readLine()!.split(separator: " ").map { ($0) }
+var a = String(input[0].reversed())
+var b = String(input[1].reversed())
 
-for i in num1.reversed(){
-    str1 += String(i)
-}
-for j in num2.reversed(){
-    str2 += String(j)
-}
-if Int(str1)! > Int(str2)! {
-    print("\(Int(str1)!)")
-}else {
-    print("\(Int(str2)!)")
-}
+print(Int(a)! > Int(b)! ? a : b)
