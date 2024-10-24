1cfd2e5a31b82f5baba06af83f21485d2ea29b68
황민채 <103357078+Hminchae@users.noreply.github.com>
2024-10-25 08:38:25 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ C#/백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int input = int.Parse(Console.ReadLine());
+        
+        int result = input - 543;
+        
+        Console.WriteLine(result);
+    }
+}
