b14aebd3f1655f40ea54a3f89a5efccd175e1ea3
황민채 <103357078+Hminchae@users.noreply.github.com>
2024-11-05 08:39:22 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ C#/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+
+        if (A > B)
+        {
+            Console.WriteLine(">");
+        }
+        else if (A < B)
+        {
+            Console.WriteLine("<");
+        }
+        else
+        {
+            Console.WriteLine("==");
+        }
+    }
+}
